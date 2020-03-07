f90255a08dfb522c2bf006c53797424dcbcef958
Matheus Abreu <abreumatheus@icloud.com>
2020-03-07 12:45:34 -0300

    Resolução desafio final. (Manda o mercadotech ae)

--- tests/test_api_cep.py
+++ tests/test_api_cep.py
@@ -5,7 +5,7 @@ from unittest.mock import patch, Mock
 
 class TestApiCep(TestCase):
     @patch('app.api_cep.re.sub', return_value='1345')
-    def test__getsomente_numeros(self, mock_sub):
+    def test__get_somente_numeros(self, mock_sub):
         dados = '1As3@45'
 
         resultado = _get_somente_numeros(dados)
@@ -20,16 +20,18 @@ class TestApiCep(TestCase):
         json_return = {'cep': '20241-250', 'logradouro': 'Rua Santa Cristina', 'complemento': '',
                        'bairro': 'Santa Teresa', 'localidade': 'Rio de Janeiro', 'uf': 'RJ', 'unidade': '',
                        'ibge': '3304557', 'gia': ''}
+        link_get = f'http://www.viacep.com.br/ws/20241250/json/'
 
         api = ApiCep()
 
-        mock_requests_get.return_value = Mock(status_code=200, json=lambda: json_return)
+        mock_requests_get.return_value = Mock(status_code=200)
+        mock_requests_get.return_value.json = Mock(return_value=json_return)
 
         response = api.execute(cep)
 
         mock_get_somente_numeros.assert_called_once_with(cep)
-
-        link_get = f'http://www.viacep.com.br/ws/20241250/json/'
         mock_requests_get.assert_called_once_with(link_get)
+        mock_requests_get.return_value.json.assert_called_once_with()
 
+        self.assertEqual(mock_requests_get.return_value.status_code, 200)
         self.assertEqual(response, json_return)
